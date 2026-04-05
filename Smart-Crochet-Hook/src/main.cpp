ea4e4cf5b9b5f30a557c3d3b936563fab01bcb5d
Alona Gertskin <alonagertskin@gmail.com>
2026-04-05 12:11:36 +0300

    moved definitions to config file

--- Smart-Crochet-Hook/src/main.cpp
+++ Smart-Crochet-Hook/src/main.cpp
@@ -1,22 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <MPU9250.h>
-// --- DEFINITIONS ---
-#define MPU_ADDR 0x68
-#define SDA_PIN 8
-#define SCL_PIN 9
-// Registers
-#define REG_PWR_MGMT_1 0x6B
-#define REG_GYRO_CONFIG 0x1B
-#define REG_ACCEL_XOUT_H 0x3B
-// Settings
-#define MPU_DATA_LEN 14
-#define GYRO_FULL_SCALE_2000DPS 0x18
-#define COMM_SPEED 115200
-
-const unsigned long INTERVAL_US = 20000; // 20,000 microseconds = 20ms (50Hz)
-unsigned long nextSampleMicros = 0;
-MPU9250 mpu;
+#include "config.h"
 
 // Function to write a single byte to a specific MPU register
 void writeRegister(uint8_t reg, uint8_t value) {
